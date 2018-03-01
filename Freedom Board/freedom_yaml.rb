57f9ea4bed7c2cc9f767afb19c328fc817adf837
twistingtiger <pulmaats@gmail.com>
2018-03-01 14:26:34 +0800

    Changed freedom_yaml.rb
    Changed yaml file. Test it nalang sean

--- Freedom Board/freedom_yaml.rb
+++ Freedom Board/freedom_yaml.rb
@@ -1,12 +1,23 @@
 require 'yaml/store'
 
-Posts = Struct.new :message, :name
+store =  YAML::Store.new "FreedomStore.yaml"
 
-post = [Posts.new("HELLO!", "Anonymus")]
+data1 = store.transaction {store[:name]}
+data2 = store.transaction {store[:message]}
 
-store =  YAML::Store.new "test.store"
+#Do something with data 1 and data 2 kun mayda man
+#like to set values nira here
+#data[:name]
+#data[:message]
 
+
+#stored
 store.transaction do
-	store ["post"] = post
-	store ["greeting"] = {"Stored" => "contents"}
+	store [:name] = data1
+	store [:message]= data2
+	store ["Freedom Board"] = {"Stored" => "contents"}
+	
+	store.abort 
+	store.commit
+	store[:time_posted]= time.now
 end
