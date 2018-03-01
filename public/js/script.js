7e71c5eeb88733133e97260cb1c35d94c80c9e11
Sean Francis N. Ballais <sean@seanballais.com>
2018-03-01 14:42:53 +0800

    Kind of completed the design for the message form. Added mobile support like legit.

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,27 @@ $(document).ready(function() {
         });
     });
 
-    $('div#floating-message-textbox-trigger').hover(
-        function() { // On mouse enter
-            $(this).stop().animate({ backgroundColor: "#7f28e2", borderColor: "#7f28e2" })
-        },
-        function() { // On mouse exit
-            $(this).stop().animate({ backgroundColor: "#5701b8", borderColor: "#5701b8" })
-        }
-    );
-
     $('div#floating-message-textbox > h3 > span#close-button').click(function() {
         $('div#floating-message-textbox').slideUp(150, function() {
             $('div#floating-message-textbox-trigger').fadeIn(150);
         });
+    });
+
+    $('article div#article-contents div#message-writer form#form-bait').focusin(function() {
+        $(this).fadeOut(150, function() {
+            $(this).next('form').slideDown(150);
+            $('article div#article-contents div#message-writer form#form-writer div div textarea').focus();
+        });
+    });
+
+    $('article div#article-contents div#message-writer form#form-writer').focusout(function() {
+        var message = $('article div#article-contents div#message-writer form#form-writer div div textarea').val();
+        var author = $('article div#article-contents div#message-writer form#form-writer div div input').val();
+
+        if (message == '' && author == '') {
+            $(this).slideUp(150, function() {
+                $(this).prev('form').fadeIn(150);
+            });
+        }
     });
 });
